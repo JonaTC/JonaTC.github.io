0fd6310b05f9f57885d6e190866079a4d3f16384
JonaTc <sc0447@unialvart.edu.mx>
2022-08-24 02:16:25 -0500

    icon added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://es.dreamstime.com/icono-de-la-cara-gui%C3%B1o-elemento-anime-face-icon-para-concepto-m%C3%B3vil-y-aplicaciones-web-wink-se-puede-usar-el-en-fondo-blanco-image188565587"} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clic para recargar pagina!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jonathantc.me"
           target="_blank"
           rel="noopener noreferrer"
         >
