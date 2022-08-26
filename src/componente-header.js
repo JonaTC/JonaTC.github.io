21366bb5eed50c200f4bea466c2442d02b95a9c3
JonaTc <sc0447@unialvart.edu.mx>
2022-08-25 23:10:07 -0500

    update

--- src/componente-header.js
+++ src/componente-header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function Componenteheader(){
-    return(
-        <nav>
-            <ul>
-                <li><a href='#'>Inicio</a></li>
-                <li><a href='#'>GitHub</a></li>
-                <li><a href='#'>Portafolio</a></li>
-                <li><a href='#'>Acerca de mi</a></li>
-            </ul>
-        </nav>
-    );
+function Componenteheader() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">GitHub</a></li>
+        <li><a href="#">Portafolio</a></li>
+        <li><a href="#">Acerca de mi</a></li>
+      </ul>
+    </nav>
+  );
 }
 export { Componenteheader };
--- src/componentebody.js
+++ src/componentebody.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './componentebody.css';
 
-function Componente(){
-    return(
-        <div>
-            <div>
-                <h1>Desarrollador Backend</h1>
-                <p>Ingeniero en Sistemas Computacionales</p>
-                <p>Amante de la del desarrllo web, Desarrrllo de aplicaciones Desktop</p>
-            </div>
-            <div>
-                <h2> Experiencia con Lenguajes de programación</h2>
-                <p>Python</p>
-                <p>Java</p>
-                <p>SQL</p>
-                <p>HTML 5</p>
-                <P>CSS 3</P>
-            </div>
-        </div>
-    );
+function Componente() {
+  return (
+    <div>
+      <div>
+        <h1>Desarrollador Backend</h1>
+        <p>Ingeniero en Sistemas Computacionales</p>
+        <p>Amante de la del desarrllo web, Desarrrllo de aplicaciones Desktop</p>
+      </div>
+      <div>
+        <h2> Experiencia con Lenguajes de programación</h2>
+        <p>Python</p>
+        <p>Java</p>
+        <p>SQL</p>
+        <p>HTML 5</p>
+        <P>CSS 3</P>
+      </div>
+    </div>
+  );
 }
 export { Componente };
